dd69acd45aafc76c322c107658da4b8b8f56868d
gnuchanos <gnuchanos@gmail.com>
2024-02-17 08:11:12 +0300

    change is change

--- src/dotfiles/programs/vim/install.sh
+++ src/dotfiles/programs/vim/install.sh
@@ -1,2 +1,9 @@
-cp -p .ycm_extra_conf.py ~/.vim/plugged/YouCompleteMe/
+mkdir -p ~/.vim
 cp .vimrc ~/
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp -p .ycm_extra_conf.py ~/.vim/plugged/YouCompleteMe/
+cd ~/.vim/plugged/YouCompleteMe/
+./install.py --clang-completer
+
+
+
