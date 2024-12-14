fab2dbad70901c4bb45905f57610d6e0af2772e7
gnuchanos <gnuchanos@gmail.com>
2024-12-14 18:29:16 +0300

    new day new update

--- src/oldInstall/install2.sh
+++ src/oldInstall/install2.sh
@@ -76,21 +76,23 @@ echo "GPU Driver"
 while true; do
     echo " | input words -> | amd | intel | nvidia | ati | exit |  :> "; read input
     if [ $input == "amd" ]; then
-        pacman -S --noconfirm xorg xorg-server xorg-xinit xorg-apps mesa xf86-video-amdgpu 
-		pacman -S --nocomfirm xf86-input-libinput vulkan-radeon lib32-vulkan-radeon
+        sudo pacman -S --noconfirm xorg xorg-server xorg-xinit xorg-apps mesa xf86-video-amdgpu 
+		sudo pacman -S --nocomfirm xf86-input-libinput vulkan-radeon lib32-vulkan-radeon
         break
     elif [ $input == "intel" ]; then
-        pacman -S --noconfirm xorg xorg-server xorg-xinit xorg-apps mesa xf86-video-intel xf86-input-libinput vulkan-intel
+        sudo pacman -S --noconfirm xorg xorg-server xorg-xinit xorg-apps mesa xf86-video-intel xf86-input-libinput vulkan-intel
         break
     elif [ $input == "nvidia" ]; then
-        pacman -S --noconfirm xorg xorg-server xorg-xinit mesa nvidia nvidia-utils nvidia-settings 
-		pacman -S --nocomfirm nvidia-dkms libglvnd vulkan-icd-loader
-		pacman -S --noconfirm xf86-input-libinput xf86-input-evdev xf86-video-vesa
-		echo "use this later -> nvidia-xconfig"
-        break
+        sudo pacman -S --noconfirm xorg xorg-server xorg-xinit mesa nvidia nvidia-utils nvidia-settings 
+		sudo pacman -S --nocomfirm nvidia-dkms libglvnd vulkan-icd-loader
+		sudo pacman -S --noconfirm xf86-input-libinput xf86-input-evdev xf86-video-vesa
+		sudo nvidia-xconfig
+		sudo systemctl enable nvidia-persistenced.service
+		sudo systemctl start nvidia-persistenced.service
+		break
     elif [ $input == "ati" ]; then
-        pacman -S --noconfirm xorg xorg-server xorg-xinit xorg-apps mesa xf86-video-ati 
-		pacman -S --nocomfirm xf86-input-libinput vulkan-radeon lib32-vulkan-radeon
+        sudo pacman -S --noconfirm xorg xorg-server xorg-xinit xorg-apps mesa xf86-video-ati 
+		sudo pacman -S --nocomfirm xf86-input-libinput vulkan-radeon lib32-vulkan-radeon
         break
     elif [ $input == "exit" ]; then
         break
@@ -101,26 +103,6 @@ done
 
 read -rsn1 -p "##################### FINISH : PRESS ENTER ########################" variable; echo
 ##################################################################################################
-
-##################################################################################################
-read -rsn1 -p "####### Display Manager ##########################################" variable; echo
-pacman -S --nocomfirm ly 
-systemctl enable ly.service
-read -rsn1 -p "##################### FINISH : PRESS ENTER ########################" variable; echo
-##################################################################################################
-
-##################################################################################################
-read -rsn1 -p "######## Gui selection ###########################################" variable; echo
-pacman -S --noconfirm qtile
-read -rsn1 -p "##################### FINISH : PRESS ENTER ########################" variable; echo
-##################################################################################################
-
-##################################################################################################
-read -rsn1 -p "####### Extra Programs ##############################################" variable; echo
-pacman -S --noconfirm xterm fish vim qutebrowser
-read -rsn1 -p "##################### FINISH : PRESS ENTER ########################" variable; echo
-##################################################################################################
-
 
 ##################################################################################################
 echo "first enter this command 1: exit 2: umount /mnt/boot and umount /mnt "
