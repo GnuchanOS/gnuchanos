fd060228e6b4dd3de133cd793369814a9ea187ad
gnuchanos <gnuchanos@gmail.com>
2025-01-13 06:25:03 +0300

    update time

--- src/Dotfiles/Programs/RunScript/vim/install.sh
+++ src/Dotfiles/Programs/RunScript/vim/install.sh
@@ -5,7 +5,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qall
 echo "cd ~/.vim/plugged/YouCompleteMe"
-echo "python3 install.py --clangd-completer --tern-completer --rust-completer"
+echo "python3 install.py --clang-completer --tern-completer --rust-completer"
 
 
 # for xterm
